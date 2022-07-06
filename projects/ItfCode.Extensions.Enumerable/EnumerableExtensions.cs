15a9ee67db97e4aa2bdc54bd557459632ad3b4d1
Vince Jacobs <vince.jacobs@bbconsult.co.uk>
2022-07-06 18:34:32 +0300

    Commit 06

--- projects/ItfCode.Extensions.Enumerable/EnumerableExtensions.cs
+++ projects/ItfCode.Extensions.Enumerable/EnumerableExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace ItfCode.Extensions.Enumerable
+﻿namespace ItfCode.Extensions.EnumerableExtendors
 {
     public static class EnumerableExtensions
     {
@@ -19,6 +19,45 @@
                 ordered = isAsc ? ordered.ThenBy(keySelectors[i]) : ordered.ThenByDescending(keySelectors[i]);
 
             return ordered;
+        }
+
+        /// <summary>
+        /// Like CROSS APPLY in MS SQL.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="keys">The keys.</param>
+        /// <param name="keySelector">The key selector.</param>
+        /// <param name="count">The count.</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> CrossApply<TSource, TKey>(this IEnumerable<TSource> source, IEnumerable<TKey> keys,
+            Func<TSource, TKey> keySelector, int count = 1)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(keys);
+            ArgumentNullException.ThrowIfNull(keySelector);
+
+            var items = Enumerable.Empty<TSource>();
+
+            foreach (TKey key in keys)
+            {
+                items = items.Union(source.Where(x => keySelector(x).Equals(key)).Take(count));
+            }
+
+            return items;
+        }
+
+        public static IReadOnlyCollection<T> ToReadOnlyCollection<T>(this IEnumerable<T> source) => source.ToList().AsReadOnly();
+
+        public static IReadOnlyList<T> ToReadOnlyList<T>(this IEnumerable<T> source) => source.ToList().AsReadOnly();
+
+        public static string JoinToSting<T>(this IEnumerable<T> source, string separator = ",")
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(separator);
+
+            return string.Join(separator, source);
         }
     }
 }
---
+++ tests/ItfCode.Extensions.Enumerable.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ItfCode.Extensions.Enumerable.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
